b0f36de31b686c9cf73afdafddf2e7fc57f99fd5
LNA <ThisIsLaToyaNicole@gmail.com>
2013-02-24 21:27:45 -0600

    Passing tests for game class

--- game.rb
+++ game.rb
@@ -1,2 +1,11 @@
 class Game
+
+	def initialize(output)
+		@output = output
+	end
+
+	def start
+		@output.puts('Welcome to Tic Tac Toe. You are player X.  Good luck!')
+		@output.puts('Enter first move: ')
+	end
 end
--- tic_tac_toe_spec.rb
+++ tic_tac_toe_spec.rb
@@ -2,5 +2,18 @@ require "./game"
 
 
 describe Game do 
+
+	let(:output) { double('output').as_null_object }
+	let(:game)   { Game.new(output) }
+
+	it "starts by greeting the player" do
+		output.should_receive(:puts).with('Welcome to Tic Tac Toe. You are player X.  Good luck!')
+		game.start 
+	end
+
+	it "prompts the first move" do 
+		output.should_receive(:puts).with('Enter first move: ')
+		game.start
+	end
 	
 end
